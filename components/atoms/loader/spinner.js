18df81fff33527d8951f727d23524e9e8280260b
SekhudinPbg3 <Sekhudinpbg3@gmail.com>
2022-11-17 17:12:01 +0700

    perbaikan

--- components/atoms/loader/spinner.js
+++ components/atoms/loader/spinner.js
@@ -1,9 +1,15 @@
-import ClipLoader from "react-Spinners/ClipLoader";
+import { ClipLoader } from "react-Spinners";
 
 const Spinner = ({ loading }) => {
   return (
     <div className={loading ? "loader-loading" : "hidden"}>
-      <ClipLoader className='grid-center-xy' loading={loading} color='#6B7280' size={20} speedMultiplier={0.8} />
+      <ClipLoader
+        className="grid-center-xy"
+        loading={loading}
+        color="#6B7280"
+        size={20}
+        speedMultiplier={0.8}
+      />
     </div>
   );
 };
